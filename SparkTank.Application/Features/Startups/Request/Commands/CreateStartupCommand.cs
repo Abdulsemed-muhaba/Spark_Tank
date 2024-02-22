b5dcb3dbd7478ae26f5e7f96917c37ab9ed05dcb
aymen-eliyas <aymen.eliyas@a2sv.org>
2024-02-22 18:34:58 +0300

    entities

---
+++ SparkTank.Application/Features/Startups/Request/Commands/CreateStartupCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using SparkTank.Application.DTOs.StartupDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkTank.Application.Features.Startups.Request.Commands
+{
+    public class CreateStartupCommand : IRequest<CreateStartupDto>
+    {
+        public CreateStartupDto? createStartupDto { get; set; }
+    }
+}
